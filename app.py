37c335604d349b408f9aff0c797da87731071191
Álvaro de Castro <alvaros8000@gmail.com>
2023-04-17 23:39:52 +0200

    New functionality
    The script has been optimized. Now, it will perform a scan in order to find the files whose 'datetime' and 'datetime_original' metadata is not the same as the date in their names before actually changing those parameters only in the files that do not fulfill said condition.

--- app.py
+++ app.py
@@ -10,8 +10,23 @@ import argparse
 from colorama import Fore
 from tqdm import trange
 
+def check_metadata(img_path:str) -> bool:
+    """
+    Check wether an image's 'datetime' and 'datetime_original' are the same
+    as the date in their name.
+    """
+    result = False
+    filename = img_path[img_path.rfind("/")+1:]
+    dst_date = datetime.datetime.strptime(f"{filename[4:12]}","%Y%m%d")
+    with open(img_path, 'rb') as img_file:
+        img = Image(img_file)
+    if (img.has_exif and ("datetime" in img.list_all()) and ("datetime_original" in img.list_all())):
+        if(img.get("datetime")[:10] == dst_date.strftime("%Y:%m:%d") and 
+        img.get("datetime")[:10] == img.get("datetime_original")[:10]):
+            result = True
+    return result
+
 def mod_metadata(img_path:str):
-    ok = True
     filename = img_path[img_path.rfind("/")+1:]
     dst_date = datetime.datetime.strptime(f"{filename[4:12]}","%Y%m%d")
     with open(img_path, 'rb') as img_file:
@@ -19,7 +34,6 @@ def mod_metadata(img_path:str):
 
     img.datetime = f"{dst_date.strftime('%Y:%m:%d %H:%M:%S')}"
     img.datetime_original = img.datetime
-    
 
     with open(f'{img_path}','wb') as img_mod_file:
         img_mod_file.write(img.get_file())
@@ -27,10 +41,23 @@ def mod_metadata(img_path:str):
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="A tool to set the 'datetime' and 'datetime_original' metadatas of mobile phone images to the date indicated by their name.")
     parser.add_argument("directory",nargs=1, help="the directory where the images are stored.")
-    folder_path = vars(parser.parse_args())["directory"][0]
-    files = glob.glob(f'{folder_path}/IMG-????????-WA*.jpg', recursive=False)
-    
+    folder_path:str = vars(parser.parse_args())["directory"][0]
 
+    files:list = glob.glob(f'{folder_path}/IMG-????????-WA*.jpg', recursive=False)
+    files_to_change:list = []
+    print(f"Found {len(files)} images")
+
+    print("Checking which images have to have their metadata changed...")
+    
     for i in trange(len(files), desc="Progress"):
-        mod_metadata(files[i])
+        try:
+            if (not check_metadata(files[i])):
+                files_to_change.append(files[i])
+        except TypeError:
+            print(f"{Fore.RED}TypeError in {files[i][files[i].rfind('/')+1:]} -> dst_date.strftime('%Y:%m:%d')={datetime.datetime.strptime(files[i][files[i].rfind('/')+1:][4:12],'%Y%m%d').strftime('%Y:%m:%d')}{Fore.RESET}")
+    
+    print(f"A total of {len(files_to_change)} files have to have their metadata changed\n"+
+          "Changing images' metadata...")
+    for i in trange(len(files_to_change), desc="Progress"):
+        mod_metadata(files_to_change[i])
     print(f"{Fore.GREEN}Done!{Fore.RESET}")
